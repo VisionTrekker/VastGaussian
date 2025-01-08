7e72bc91f025d9ec0fb9cdb93bf9af3d95d70de1
lzhi <18082258131@163.com>
2025-01-09 00:31:41 +0800

    comment at partition_merge

--- seamless_merging.py
+++ seamless_merging.py
@@ -20,31 +20,69 @@ from scene.dataset_readers import storePly
 
 
 def extend_inf_x_z_bbox(partition_id, m_region, n_region):
-    # bbox: [x_min, x_max, z_min, z_max]
-    # m_region and n_region must >= 2
-    # 进行无缝合并，需要根据partition所在位置作战original bbox的x z轴的范围，从而进行无缝合并后包含背景元素
-    x, z = int(partition_id.split("_")[0]), int(partition_id.split("_")[1])  # 获取块所在编号
-    if x == 1 and z == 1:  # 左上角
+    """
+    根据分块位置，沿X或Z轴扩展当前分块camera_bbox的范围，以便进行无缝合并
+        partition_id (str): 分块编号，例如 "1_1"
+        m_region (int): X轴最大编号
+        n_region (int): Z轴最大编号
+    Returns:
+        list[bool]: 对应表示是否需要扩展 [x_min, x_max, z_min, z_max] 为inf
+    """
+    x, z = int(partition_id.split("_")[0]), int(partition_id.split("_")[1])  # 块的X,Z编号
+    if n_region == 1:   # 单行
+        if x == 1:  # 最左端
+            return [True, False, True, True]
+        elif x == m_region:  # 最右端
+            return [False, True, True, True]
+        else:  # 中间
+            return [False, False, True, True]
+    if m_region == 1:  # 单列
+        if z == 1:  # 最下端
+            return [True, True, True, False]
+        elif z == n_region:  # 最上端
+            return [True, True, False, True]
+        else:  # 中间
+            return [True, True, False, False]
+
+    # 常规情况 (m_region >= 2 and n_region >= 2)
+    if x == 1 and z == 1:  # 左下角
         return [True, False, True, False]
-    if x == m_region and z == 1:  # 右上角
+    if x == m_region and z == 1:  # 右下角
         return [False, True, True, False]
-    if x == 1 and z == n_region:  # 左下角
+    if x == 1 and z == n_region:  # 左上角
         return [True, False, False, True]
-    if x == m_region and z == n_region:  # 右下角
+    if x == m_region and z == n_region:  # 右上角
         return [False, True, False, True]
-    if 2 <= x <= m_region-1 and z == 1:  # 最上边中间
+    if 2 <= x <= m_region - 1 and z == 1:  # 最下边中间
         return [False, False, True, False]
-    if 2 <= z <= n_region-1 and x == 1:  # 最左边中间
+    if 2 <= z <= n_region - 1 and x == 1:  # 最左边中间
         return [True, False, False, False]
-    if 2 <= x <= m_region-1 and z == n_region:  # 最下边中间
+    if 2 <= x <= m_region - 1 and z == n_region:  # 最上边中间
         return [False, False, False, True]
-    if 2 <= z <= n_region-1 and x == m_region:  # 最右边中间
+    if 2 <= z <= n_region - 1 and x == m_region:  # 最右边中间
         return [False, True, False, False]
-    if 2 <= x <= m_region-1 and 2 <= z <= n_region-1:  # 中间
+    if 2 <= x <= m_region - 1 and 2 <= z <= n_region - 1:  # 中间
         return [False, False, False, False]
 
+    # # 简洁版：
+    # x, z = map(int, partition_id.split("_"))  # 分块编号的X和Z
+    # # 初始化扩展标志
+    # extend_x_min = (x == 1)  # 最左列扩展x_min
+    # extend_x_max = (x == m_region)  # 最右列扩展x_max
+    # extend_z_min = (z == 1)  # 最下行扩展z_min
+    # extend_z_max = (z == n_region)  # 最上行扩展z_max
+    # # 处理单行和单列情况
+    # if n_region == 1:  # 单行，扩展z_min和z_max
+    #     extend_z_min = True
+    #     extend_z_max = True
+    # if m_region == 1:  # 单列，扩展x_min和x_max
+    #     extend_x_min = True
+    #     extend_x_max = True
+    #
+    # return [extend_x_min, extend_x_max, extend_z_min, extend_z_max]
 
-def load_ply(path):
+
+def load_ply(path): # 加载高斯点云
     plydata = PlyData.read(path)
     max_sh_degree = 3
     xyz = np.stack((np.asarray(plydata.elements[0]["x"]),
@@ -82,41 +120,45 @@ def load_ply(path):
 
 
 def extract_point_cloud(points, bbox):
-    """根据camera的边界从初始点云中筛选对应partition的点云"""
-    mask = (points[:, 0] >= bbox[0]) & (points[:, 0] <= bbox[1]) & (
-            points[:, 1] >= bbox[2]) & (points[:, 1] <= bbox[3]) & (
-                   points[:, 2] >= bbox[4]) & (points[:, 2] <= bbox[5])  # 筛选在范围内的点云，得到对应的mask
+    """根据当前partition的camera_bbox，从当前分块的高斯点云中 筛选出在范围内的点云的mask"""
+    mask = ((points[:, 0] >= bbox[0]) & (points[:, 0] <= bbox[1]) &
+            (points[:, 1] >= bbox[2]) & (points[:, 1] <= bbox[3]) &
+            (points[:, 2] >= bbox[4]) & (points[:, 2] <= bbox[5]))
     return mask
 
 
 def seamless_merge(model_path, partition_point_cloud_dir):
+    """
+    将 partition_point_cloud_dir（iteration_xxx）文件夹下的 x_x_point_cloud.ply 合并为一个point_cloud.ply
+    """
     save_merge_dir = os.path.join(partition_point_cloud_dir, "point_cloud.ply")
 
     # 加载partition数据
     with open(os.path.join(model_path, "partition_data.pkl"), "rb") as f:
         partition_scene = pickle.load(f)
 
+    # 获取分成的块数 x_x
     m_region, n_region = 0, 0
-    # 获取分成了多少块
     for partition in partition_scene:
         m, n = int(partition.partition_id.split("_")[0]), int(partition.partition_id.split("_")[1])
         if m > m_region: m_region = m
         if n > n_region: n_region = n
 
-    # 遍历所有partition点云
+    # 存储要合并的高斯点云各属性的列表
     xyz_list = []
     features_dc_list = []
     features_extra_list = []
     opacities_list = []
     scales_list = []
     rots_list = []
-
-    for partition in partition_scene:
+    # 遍历所有partition点云
+    for partition in partition_scene:   # 遍历iteration_xxx文件夹下的 多个分块点云 x_x_point_cloud.ply
         point_cloud_path = os.path.join(partition_point_cloud_dir, f"{partition.partition_id}_point_cloud.ply")
-        if not os.path.exists(point_cloud_path): continue
-        xyz, features_dc, features_extra, opacities, scales, rots = load_ply(point_cloud_path)
-        ori_camera_bbox = partition.ori_camera_bbox
-        extend_camera_bbox = partition.extend_camera_bbox  # 原始相机包围盒
+        if not os.path.exists(point_cloud_path):
+            continue
+        xyz, features_dc, features_extra, opacities, scales, rots = load_ply(point_cloud_path)  # 加载高斯点云
+        ori_camera_bbox = partition.ori_camera_bbox         # 原始相机bbox
+        extend_camera_bbox = partition.extend_camera_bbox   # 拓展相机bbox
         # x_max = extend_camera_bbox[1]
         # x_min = extend_camera_bbox[0]
         # z_max = extend_camera_bbox[3]
@@ -126,6 +168,7 @@ def seamless_merge(model_path, partition_point_cloud_dir):
         z_max = ori_camera_bbox[3]
         z_min = ori_camera_bbox[2]
 
+        # 根据当前分块在整个场景的位置，沿X或Z轴扩展原始camera_bbox的边界为inf
         flag = extend_inf_x_z_bbox(partition.partition_id, m_region, n_region)
         # if partition.partition_id == "1_1":
         #     flag = [True, False, True, True]
@@ -139,20 +182,18 @@ def seamless_merge(model_path, partition_point_cloud_dir):
 
         print('region:', point_cloud_path)
         print('x_min:{}, x_max:{}, z_min:{}, z_max:{}'.format(x_min, x_max, z_min, z_max))
-        
-        point_select_bbox = [x_min, x_max,  # [x_min, x_max, y_min, y_max, z_min, z_max]
-                             -np.inf, np.inf,
-                             # 考虑原始点云的包围盒的y轴范围作为还原的范围，因为在partition时，没有考虑y轴方向
-                             z_min, z_max]
-        
-        mask = extract_point_cloud(xyz, point_select_bbox)
+
+        point_select_bbox = [x_min, x_max, -np.inf, np.inf, z_min, z_max]   # 3D bbox
+        mask = extract_point_cloud(xyz, point_select_bbox)  # 筛选处在camera_bbox内的高斯点云
+        # 添加
         xyz_list.append(xyz[mask])
         features_dc_list.append(features_dc[mask])
         features_extra_list.append(features_extra[mask])
         opacities_list.append(opacities[mask])
         scales_list.append(scales[mask])
         rots_list.append(rots[mask])
-        
+
+
         fig, ax = plt.subplots()
         x_pos = xyz[mask][:, 0]
         z_pos = xyz[mask][:, 2]
